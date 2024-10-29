b2961f85ff294c9d493f5306ffcab7b31c446cfc
Your Name <dmmuchai@gmail.com>
2024-10-29 04:57:50 -0700

    Task 2. Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,28 +7,27 @@
  */
 void selection_sort(int *array, size_t size_arr)
 {
-	size_t i, j, min_idx;
-	int tmp;
-
-	if (array == NULL || size_arr < 2)
-		return;
+	size_t i, j;
+	int min, tmp, idx;
 
 	for (i = 0; i < size_arr - 1; i++)
 	{
-		min_idx = i;
-
+		min = array[i];
 		for (j = i + 1; j < size_arr; j++)
 		{
-			if (array[j] < array[min_idx])
-			min_idx = j;
+			if (min > array[j])
+			{
+				min = array[j];
+				idx = j;
+			}
 		}
-
-		if (min_idx != i)
+		if (min != array[i])
 		{
 			tmp = array[i];
-			array[i] = array[min_idx];
-			array[min_idx] = tmp;
+			array[i] = min;
+			array[idx] = tmp;
 			print_array(array, size_arr);
 		}
 	}
+
 }
