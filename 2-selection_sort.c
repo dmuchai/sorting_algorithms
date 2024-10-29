8280d167c3b06ea027865a84c14030e17c1195fb
Your Name <dmmuchai@gmail.com>
2024-10-29 05:36:18 -0700

    Task 2. Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,33 +1,33 @@
 #include "sort.h"
 /**
- * selection_sort - Sorts an array of integers in ascending order
- * using the Selection sort algorithm
+ * selection_sort - Sorts array of integers using Selection sort algorithm
  * @array: Array of integers to sort
  * @size: Size of the array
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int min, tmp, idx;
+	size_t i, j, min_idx;
+	int tmp;
 
-	for (i = 0; i < size; i++)
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
 	{
-		min = array[i];
+		min_idx = i;
+
 		for (j = i + 1; j < size; j++)
 		{
-			if (min > array[j])
-			{
-				min = array[j];
-				idx = j;
-			}
+			if (array[j] < array[min_idx])
+			min_idx = j;
 		}
-		if (min != array[i])
+
+		if (min_idx != i)
 		{
 			tmp = array[i];
-			array[i] = min;
-			array[idx] = tmp;
+			array[i] = array[min_idx];
+			array[min_idx] = tmp;
 			print_array(array, size);
 		}
 	}
 }
-
