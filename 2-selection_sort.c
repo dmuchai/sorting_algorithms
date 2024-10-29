c287407d6aa41a9b33dc5a6f76066a557831014c
Your Name <dmmuchai@gmail.com>
2024-10-28 22:56:34 -0700

    Task 2. Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,33 +1,36 @@
 #include "sort.h"
-/**
- * selection_sort - function that sorts an array of integers in ascending
- * order using the Selection sort algorithm
- * @size: size of the array
- * @array: list with numbers
- */
-void selection_sort(int *array, size_t size)
-{
-	size_t i, index;
-	int tmp, swap, flag = 0;
+#include <stdio.h>
 
-	if (array == NULL)
+/**
+ * selection_sort - Sorts an array of integers in ascending order
+ * using the Selection sort algorithm
+ * @array: Array of integers to sort
+ * @size_arr: Size of the array
+ */
+void selection_sort(int *array, size_t size_arr)
+{
+	size_t i, j, min_idx;
+	int tmp;
+
+	if (array == NULL || size_arr < 2)
 		return;
-	for (i = 0; i < size; i++)
+
+	for (i = 0; i < size_arr - 1; i++)
 	{
-		tmp = i;
-		flag = 0;
-		for (index = i + 1; index < size; index++)
+		min_idx = i;
+
+		for (j = i + 1; j < size_arr; j++)
 		{
-			if (array[tmp] > array[index])
-			{
-				tmp = index;
-				flag += 1;
-			}
+			if (array[j] < array[min_idx])
+			min_idx = j;
 		}
-		swap = array[i];
-		array[i] = array[tmp];
-		array[tmp] = swap;
-		if (flag != 0)
-			print_array(array, size);
+
+		if (min_idx != i)
+		{
+			tmp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = tmp;
+			print_array(array, size_arr);
+		}
 	}
 }
