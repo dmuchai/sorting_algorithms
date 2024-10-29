acb9c2d0c830c8eb4859ce1738e54861934a9548
Your Name <dmmuchai@gmail.com>
2024-10-29 06:19:56 -0700

    Task 2- select

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+
 /**
  * selection_sort - Sorts array of integers using Selection sort algorithm
  * @array: Array of integers to sort
@@ -6,27 +7,28 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_idx;
+	size_t i, j, min_x;
 	int tmp;
 
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		min_idx = i;
-
+		min_x = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min_idx])
-			min_idx = j;
+			if (array[j] < array[min_x])
+			{
+				min_x = j;
+			}
 		}
 
-		if (min_idx != i)
+		if (min_x != i)
 		{
 			tmp = array[i];
-			array[i] = array[min_idx];
-			array[min_idx] = tmp;
+			array[i] = array[min_x];
+			array[min_x] = tmp;
 			print_array(array, size);
 		}
 	}
