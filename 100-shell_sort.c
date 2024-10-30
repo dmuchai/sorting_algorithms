1c1180cf8fb2c1388141a43f705fa5d28b806bf1
Your Name <dmmuchai@gmail.com>
2024-10-30 12:18:46 -0700

    Task 4. Shell sort - Knuth Sequence

---
+++ 100-shell_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+/**
+ * shell_sort - Sorts array using Shell sort algorithm the Knuth sequence.
+ * @array: The array of integers.
+ * @size: The size of the array.
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 1;
+	size_t i, j;
+
+	while (gap < size / 3)
+	gap = gap * 3 + 1;
+
+	while (gap >= 1)
+	{
+		for (i = gap; i < size; i++)
+		{
+			int temp = array[i];
+
+			j = i;
+
+			while (j >= gap && array[j - gap] > temp)
+			{
+				array[j] = array[j - gap];
+				j -= gap;
+			}
+			array[j] = temp;
+		}
+
+		print_array(array, size);
+
+		gap = (gap - 1) / 3;
+	}
+}
+
